931e7494a543b78caf12135f77f62758167f983e
Vitor Cardoso <vitor.c.lemes@aluno.senai.br>
2025-11-27 15:22:37 -0300

    final

--- db_BACKUP/academia_db.sql
+++ db_BACKUP/academia_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 05/11/2025 às 23:33
+-- Tempo de geração: 27/11/2025 às 19:21
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -34,6 +34,14 @@ CREATE TABLE `alunos` (
   `senha` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Despejando dados para a tabela `alunos`
+--
+
+INSERT INTO `alunos` (`id`, `nome`, `email`, `senha`) VALUES
+(15, 'Adalto', 'adalto@gmail.com', '$2y$10$c8EELFsOO75tU8QO.vT8l.ubQsREr4VUu8lO5QyZHylWJ5kQ46MTi'),
+(21, 'jamal', 'jamal@gmail.com', '$2y$10$t4uWRlbC6ihKT0ACVgA7GOgXOCZUWx6eFCvz.lHrUAU0ZTNwqN05a');
+
 -- --------------------------------------------------------
 
 --
@@ -52,7 +60,9 @@ CREATE TABLE `professores` (
 --
 
 INSERT INTO `professores` (`id`, `nome`, `email`, `senha`) VALUES
-(4, 'Pérola', 'perola@gmail.com', '$2y$10$3qwM/kl1S5uBN6AML7/m1OdQgx7v8Vu9TgqpSJRZNyXJywOeXki9y');
+(8, 'Pedro', 'pedro@gmail.com', '$2y$10$eNMfIsvxVjCsCIbX8nacwuQ5ywzsK9jec/l4xGWrrjbid7ozBepZG'),
+(9, 'Arthur', 'arthur@gmail.com', '$2y$10$iW/7V2z3N6/kidQJ4y3vIeMNtQBQmQ2G5wIrHKXB6NdPENQ/4rZBC'),
+(10, 'Vitor', 'vitor@gmail.com', '$2y$10$rdox0FO0EoQcQ1GNpB7BwOLtshJOH4dpT6TXMoiU5WTwm1svnTctm');
 
 -- --------------------------------------------------------
 
@@ -74,7 +84,9 @@ CREATE TABLE `treinos` (
 --
 
 INSERT INTO `treinos` (`id`, `aluno_nome`, `professor_id`, `descricao`, `modalidade`, `data_criacao`) VALUES
-(3, 'Adalto', '4', '555', 'Spinning', '2025-11-04 20:00:00');
+(15, 'boki', '4', 'Treino fofo', 'Funcional', '2025-11-10 09:33:50'),
+(17, 'gabriel', '5', '..', 'Musculação', '2025-11-10 10:58:48'),
+(21, 'jamal', '4', 'Rapido de grande intensidade', 'Musculação', '2025-11-24 08:34:51');
 
 --
 -- Índices para tabelas despejadas
@@ -106,19 +118,19 @@ ALTER TABLE `treinos`
 -- AUTO_INCREMENT de tabela `alunos`
 --
 ALTER TABLE `alunos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT de tabela `professores`
 --
 ALTER TABLE `professores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de tabela `treinos`
 --
 ALTER TABLE `treinos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
